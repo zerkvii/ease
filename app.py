9b181be905af3f339c5a1a6f8db37fd9e799639a
lk <934991794@qq.com>
2018-10-23 23:30:16 +0800

    sec

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/index')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/')
+def demo_23():
+    return render_template('demo_23.html')
+
+
+@app.route('/answer')
+def answer():
+    return render_template('answer.html')
 
 
 if __name__ == '__main__':
